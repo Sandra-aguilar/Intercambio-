848d608e36ab54801d3d4ecbe3df40dfed171711
Sandra-aguilar <62567980+Sandra-aguilar@users.noreply.github.com>
2020-07-03 04:18:27 -0500

    Add files via upload

---
+++ Invertirfilas.java
@@ -0,0 +1,13 @@
+package invertirfilas;
+
+public class Invertirfilas {
+
+    public static void main(String[] args) {
+    //Declaro Objeto de mi Clase Matriz
+       Matriz e1= new Matriz();
+       
+       e1.solicitar();
+       e1.hacer();
+    }
+    
+}
